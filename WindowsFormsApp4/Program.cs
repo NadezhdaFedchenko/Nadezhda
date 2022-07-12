4d6ed93bc356cca71697754013f8e45d65be8e22
NadezhdaFedchenko <108911111+NadezhdaFedchenko@users.noreply.github.com>
2022-07-12 16:25:28 +0700

    Update Program.cs

--- WindowsFormsApp4/Program.cs
+++ WindowsFormsApp4/Program.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApp4удалить
         [STAThread]
         static void Main()
         {
+            //коментарий 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
